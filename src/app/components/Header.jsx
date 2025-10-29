381bf2338ae56ae5acd693d4c007dd7ea984e056
srsohan <sr.sohan088@gmail.com>
2025-10-28 21:59:53 -0700

    update logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,7 +7,7 @@ import {
   LogOut,
 } from "lucide-react";
 import Image from "next/image";
-import logo from "@/assets/mlkLogo.png";
+import logo from "@/assets/logoMlk.png";
 import { useAuth } from "@/contexts/AuthContext";
 
 export default function Header() {
@@ -122,14 +122,7 @@ export default function Header() {
         <div className="hidden md:flex flex-col w-full items-center space-y-4">
           {/* Logo */}
           <div className="text-2xl font-bold text-center">
-            <Link href="/" className="group relative">
-              <div className="flex items-center gap-2">
-            <svg className="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z"/>
-            </svg>
-            <span className="text-4xl font-semibold tracking-wide">MLKPHONE</span>
-          </div>
-            </Link>
+           <Image className="cursor-pointer" src={logo} alt="MLKPHONE" width={100} height={100} />
           </div>
 
           {/* Navigation */}
