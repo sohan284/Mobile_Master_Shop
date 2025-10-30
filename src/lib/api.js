7196f3e24fe2e89d2cc27f7a19a85e424b8e2375
srsohan <sr.sohan088@gmail.com>
2025-10-29 20:51:44 -0700

    update url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://save-co.lumivancelabs.com';
+const BASE_URL = 'https://save-co.lumivancelabs.com';
 
 // Create axios instance
 const apiClient = axios.create({
